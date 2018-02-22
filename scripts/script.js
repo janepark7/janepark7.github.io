c02e7513234ed37ebfe4e7a540487116f0dfaf1b
janepark7 <janeparkjp@gmail.com>
2018-02-21 21:17:34 -0500

    added resume and script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 /* globals $, SC */
+
+$(document).ready(function() {
+	$(".up").click(function() {
+		$('html, body').animate({ "scrollTop": $("#hero").offset().top }, 1000);
+		return false;
+	});
+});
