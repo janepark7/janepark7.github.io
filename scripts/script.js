4fcc94656e465edb14493be495a1a4a772de58a5
janepark7 <janeparkjp@gmail.com>
2019-09-03 09:10:01 -0400

    fixed typo

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,3 @@
 // $(document).ready(funtion(){
 // 	$("body").hide().fadeIn(2000);
 // });
-
-// $(document).ready(function() {
-// /* ! Fades in page on load */
-// 	$('html').fadeIn(2000);
-// });
-
